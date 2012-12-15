e7219a08d134d8446db2cc167c93538dc5ab35b8
Arkadiusz Holko <fastred@fastred.org>
2012-12-15 13:14:52 +0100

    Fixes answer 4_23

--- 4_23.scm
+++ 4_23.scm
@@ -2,8 +2,8 @@
 ; Procedure from the text analyzes all expressions, then checks how many expressions
 ; there's and if there's only one expression returns it.
 ; Alyssa's procedure also analyzes all expressions, but returns the procedure
-; that takes 'environment' as an argument. This procedure checks the length
-; of the sequence when it is evaluated.
+; that takes 'environment' as an argument. This procedure during evaluation sees that
+; there's only one expression and evaluates it.
 ;
 ; b) many expressions
 ; If there's only one expression the difference is minor.
